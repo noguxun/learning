4c5543d8424410363ccebb504303731dffac12b1
Xun Gu <noguxun@gmail.com>
2013-10-12 19:13:32 +0900

    adding more examples parameter passing

--- python_c/python_tst1.py
+++ python_c/python_tst1.py
@@ -1,4 +1,27 @@
-from ctypes import cdll
+from ctypes import *
+
+# Reference:
+#  http://docs.python.org/3.3/library/ctypes.html
+
 lib = cdll.LoadLibrary('./libtst1.so')
 
 lib.PrintHello()
+lib.PrintMyWord(b"Come back to me, my dear cat")
+lib.PrintMyWord(c_char_p(b'Show me your hands'))
+
+
+some_int = lib.GetInt1( 3 );
+print(some_int)
+
+some_int = lib.GetInt2( c_int(5), c_int(5) );
+print(some_int)
+
+lib.GetString.restype = c_char_p
+binary_string = lib.GetString()
+print(binary_string)
+print(binary_string.decode('utf-8'))
+
+
+
+
+
--- python_c/tst1.c
+++ python_c/tst1.c
@@ -2,5 +2,26 @@
 
 void PrintHello(void)
 {
-    printf("\n Hello, Matrix \n");	
+    printf("Hello, Matrix \n");	
+}
+
+void PrintMyWord(char *p)
+{
+    printf("%s \n", p);	
+}
+
+
+int GetInt1(int input)
+{
+	return input + 100;
+}
+
+int GetInt2(int input1, int input2)
+{
+	return input1 + input2;
+}
+
+char* GetString(void)
+{
+	return "Dont buy shit we dont need";
 }
