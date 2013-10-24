9fb335f5c2bea6bdd891b2f1eeccc19515bb8e7f
Xun Gu <noguxun@gmail.com>
2013-10-24 16:44:59 +0900

    file orgnaization change

--- ata_direct/kernel_module/lax_sff.h
+++ ata_direct/kernel_module/lax_sff.h
@@ -1,6 +1,7 @@
-#ifndef _LAX_MODULE_H
-#define _LAX_MODULE_H
+#ifndef _LAX_SFF_H
+#define _LAX_SFF_H
 
+#include <linux/fs.h>
 #include <linux/compiler.h>
 
 struct lax_io_rext_pio {
@@ -8,5 +9,11 @@ struct lax_io_rext_pio {
 	unsigned long block;
 	void __user *buf;
 };
+
+
+long ata_sff_file_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+int ata_sff_file_open(struct inode *inode, struct file *filp);
+int ata_sff_file_release(struct inode *inode, struct file *filp);
+void ata_sff_set_para(int ctl_i, int port_i, int lax_major, int lax_minor);
 
 #endif
