b26d269e6b45f1b74588e94f05686e5fa590af7a
Xun Gu <noguxun@gmail.com>
2013-10-29 09:08:55 +0900

    no compile error for no data cmd implementation

--- ata_direct/kernel_module/lax_ahci.c
+++ ata_direct/kernel_module/lax_ahci.c
@@ -39,32 +39,19 @@ struct lax_ahci {
 
 static struct lax_ahci lax = { NULL, NULL };
 
-
-void ata_ahci_set_para(int port_i)
+static void __iomem * get_base(void)
 {
-	lax.port_index = port_i;
+	struct lax_port *port = &(lax.ports[lax.port_index]);
+
+	return port->ioport_base;
 }
 
-long ata_ahci_file_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+static void ahci_set_irq(u32 irq_max)
 {
-	long retval = 0;
-
-	if (cmd == 0xFF) {
-		/* only support VU command */
-		//ata_cmd_uv_issue(arg);
-	}
-	else  if(cmd == ATA_CMD_PIO_READ_EXT){
-		//ata_cmd_rext_pio(arg);
-	}
-	else {
-		printk(KERN_WARNING "not supported command 0x%x", cmd);
-		retval = -EPERM;
-	}
-
-	return 0;
+	iowrite32(irq_max, get_base() + PORT_IRQ_MASK);
 }
 
-void ata_tf_to_fis(const struct ata_taskfile *tf, u8 pmp, int is_cmd, u8 *fis)
+static void ata_tf_to_fis(const struct ata_taskfile *tf, u8 pmp, int is_cmd, u8 *fis)
 {
 	fis[0] = 0x27;			/* Register - Host to Device FIS */
 	fis[1] = pmp & 0xf;		/* Port multiplier number*/
@@ -107,7 +94,7 @@ static void ahci_fill_cmd_slot(struct lax_port *port,unsigned int tag, u32 opts)
 	port->cmd_slot[tag].tbl_addr_hi = cpu_to_le32((cmd_tbl_dma >> 16) >> 16);
 }
 
-static void ahci_cmd_prep_nodata(const struct ata_taskfile *tf, unsigned int tag)
+static void ahci_cmd_prep_nodata(const struct ata_taskfile *tf, unsigned int tag, u32 cmd_opts)
 {
 	void * cmd_tbl;
 	unsigned int n_elem;
@@ -127,7 +114,7 @@ static void ahci_cmd_prep_nodata(const struct ata_taskfile *tf, unsigned int tag
 	/*
 	 * Fill in command slot information.
 	 */
-	opts = cmd_fis_len | n_elem << 16 | (0 << 12);
+	opts = cmd_fis_len | n_elem << 16 | (0 << 12) | cmd_opts;
 	if (tf->flags & ATA_TFLAG_WRITE) {
 		opts |= AHCI_CMD_WRITE;
 	}
@@ -135,22 +122,155 @@ static void ahci_cmd_prep_nodata(const struct ata_taskfile *tf, unsigned int tag
 	ahci_fill_cmd_slot(port, tag, opts);
 }
 
-static void ahci_cmd_issue_nodata(unsigned int tag)
+static bool ahci_busy_wait(u32 wait_msec, void __iomem *reg, u32 mask, u32 val)
 {
-	struct lax_port *port = &(lax.ports[lax.port_index]);
+	u32 i = 0;
+	u32 reg_val;
+	bool condition = false;
 
-	writel(1 << tag, port->ioport_base + PORT_CMD_ISSUE);
+	do {
+		reg_val = ioread32(reg);
+
+		if((reg_val & mask) != val) {
+			msleep(1);
+		}
+		else {
+			break;
+			condition = true;
+		}
+		i ++;
+	} while (i < wait_msec);
+
+	if(!condition){
+		printk(KERN_ERR "wait failed on %d msec wait\n", wait_msec );
+	  	return false;
+	}
+	else {
+		return true;
+	}
 }
 
-static int ahci_init_port(int portno)
+static void ahci_fill_info(u8 cmd, u16 feature, u32 lba, u16 block, struct ata_taskfile *tf)
+{
+	union u32_b {
+		u32  data;
+		u8   b[4];
+	}u32_data;
+
+	union u16_b {
+		u16 data;
+		u8   b[4];
+	}u16_data;
+
+	tf->command = cmd;
+
+	u32_data.data = lba;
+	tf->hob_lbah = 0;
+	tf->hob_lbam = 0;
+	tf->hob_lbal = u32_data.b[3];
+	tf->lbah = u32_data.b[2];
+	tf->lbam = u32_data.b[1];
+	tf->lbal = u32_data.b[0];
+
+	u16_data.data = block;
+	tf->hob_nsect = u16_data.b[1];
+	tf->nsect = u16_data.b[0];
+
+	u16_data.data = feature;
+	tf->hob_feature = u16_data.b[1];
+	tf->feature = u16_data.b[0];
+
+	tf->device = 0xa0 | 0x40; /* using lba, copied code from hparm*/
+	tf->feature = 0x0;
+
+	tf->flags = ATA_TFLAG_ISADDR | ATA_TFLAG_LBA48 | ATA_TFLAG_DEVICE;
+}
+
+static void ahci_tf_init(struct ata_taskfile *tf)
+{
+	memset(tf, 0, sizeof(*tf));
+
+	tf->device = ATA_DEVICE_OBS;
+	tf->ctl = ATA_DEVCTL_OBS;     // obsolete bit in device control
+}
+
+static void ahci_exec_nodata(unsigned int tag, u8 command, u16 feature)
+{
+	struct ata_taskfile tf;
+	void __iomem * reg_issue = get_base() + PORT_CMD_ISSUE;
+	u32 bit_pos = 1 << tag;
+
+
+	ahci_tf_init(&tf);
+	ahci_fill_info(command, feature, 0, 0, &tf);
+
+	ahci_cmd_prep_nodata(&tf, 0, AHCI_CMD_CLR_BUSY);
+
+	iowrite32(bit_pos, reg_issue);
+
+	if(ahci_busy_wait(100, reg_issue, bit_pos, bit_pos) == false) {
+		printk(KERN_ERR "timeout exec command 0x%x\n", command);
+	}
+}
+
+static void ahci_port_start(void)
+{
+	/* power up */
+	u32 cmd, cap, tmp;
+	void __iomem *port_mmio = get_base();
+	struct lax_port *port = &lax.ports[lax.port_index];
+
+	cap = ioread32(lax.iohba_base + HOST_CAP);
+
+	cmd = ioread32(port_mmio + PORT_CMD) & ~ PORT_CMD_ICC_MASK;
+	if(cap & HOST_CAP_SSS) {
+		cmd |= PORT_CMD_SPIN_UP;
+		iowrite32(cmd, port_mmio + PORT_CMD);
+	}
+	iowrite32(cmd | PORT_CMD_ICC_ACTIVE, port_mmio + PORT_CMD);
+
+
+	/* enable fis reception */
+	/* set FIS registers */
+	if (cap & HOST_CAP_64) {
+		iowrite32((port->cmd_slot_dma >> 16) >> 16, port_mmio + PORT_LST_ADDR_HI);
+	}
+	iowrite32(port->cmd_slot_dma & 0xffffffff, port_mmio + PORT_LST_ADDR);
+
+	if (cap & HOST_CAP_64) {
+		iowrite32((port->rx_fis_dma >> 16) >> 16, port_mmio + PORT_FIS_ADDR_HI);
+	}
+	iowrite32(port->rx_fis_dma & 0xffffffff, port_mmio + PORT_FIS_ADDR);
+
+	/* enable FIS reception */
+	tmp = ioread32(port_mmio + PORT_CMD);
+	tmp |= PORT_CMD_FIS_RX;
+	iowrite32(tmp, port_mmio + PORT_CMD);
+
+	/* flush */
+	ioread32(port_mmio + PORT_CMD);
+
+
+	/* start engine */
+	tmp = ioread32(port_mmio + PORT_CMD);
+	tmp |= PORT_CMD_START;
+	iowrite32(tmp, port_mmio + PORT_CMD);
+	ioread32(port_mmio + PORT_CMD); /* flush */
+
+}
+
+
+static int ahci_init_port(void)
 {
 	/* no FBS(fis based switching) support*/
 	dma_addr_t mem_dma;
 	void * mem;
 	size_t dma_sz;
-	struct lax_port *port = &lax.ports[portno];
+	struct lax_port *port = &lax.ports[lax.port_index];
 
-	port->ioport_base = lax.iohba_base + 0x100 + (portno * 0x80);
+	printk(KERN_INFO "Init port 0x%x", lax.port_index);
+
+	port->ioport_base = lax.iohba_base + 0x100 + (lax.port_index * 0x80);
 
 	dma_sz = AHCI_PORT_PRIV_DMA_SZ;
 	mem = dma_alloc_coherent(&(lax.pdev->dev), dma_sz, &mem_dma, GFP_KERNEL);
@@ -175,6 +295,37 @@ static int ahci_init_port(int portno)
 	port->cmd_tbl = mem;
 	port->cmd_tbl_dma = mem_dma;
 
+	// TODO: investigate if reset and init controller is needed or not
+	ahci_port_start();
+
+	ahci_set_irq(0x0);
+
+	return 0;
+}
+
+
+void ata_ahci_set_para(int port_i)
+{
+	lax.port_index = port_i;
+}
+
+long ata_ahci_file_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	long retval = 0;
+
+	if (cmd == 0xFF) {
+		/* only support VU command */
+		ahci_exec_nodata(0, cmd, arg);
+
+	}
+	else  if(cmd == ATA_CMD_PIO_READ_EXT){
+		//ata_cmd_rext_pio(arg);
+	}
+	else {
+		printk(KERN_WARNING "not supported command 0x%x", cmd);
+		retval = -EPERM;
+	}
+
 	return 0;
 }
 
@@ -183,7 +334,6 @@ int ahci_init(void)
 	struct pci_dev *pdev = NULL;
 	bool found = false;
 	void __iomem * const * iomap;
-	int i;
 
 	if(lax.iohba_base) {
 		return 0;
@@ -211,16 +361,14 @@ int ahci_init(void)
 	iomap = pcim_iomap_table(lax.pdev);
 	lax.iohba_base = iomap[AHCI_PCI_BAR_STANDARD];
 
-	for(i = 0; i < AHCI_MAX_PORTS; i++) {
-		ahci_init_port(i);
-	}
+	ahci_init_port();
 
 	return 0;
 }
 
 int ata_ahci_file_open(struct inode *inode, struct file *filp)
 {
-
+	ahci_init();
 	return 0;
 }
 
--- ata_direct/kernel_module/lax_ahci.h
+++ ata_direct/kernel_module/lax_ahci.h
@@ -40,6 +40,30 @@ enum {
 	HOST_EM_CTL		= 0x20, /* Enclosure Management Control */
 	HOST_CAP2		= 0x24, /* host capabilities, extended */
 
+	/* HOST_CTL bits */
+	HOST_RESET		= (1 << 0),  /* reset controller; self-clear */
+	HOST_IRQ_EN		= (1 << 1),  /* global IRQ enable */
+	HOST_AHCI_EN		= (1 << 31), /* AHCI enabled */
+
+	/* HOST_CAP bits */
+	HOST_CAP_SXS		= (1 << 5),  /* Supports External SATA */
+	HOST_CAP_EMS		= (1 << 6),  /* Enclosure Management support */
+	HOST_CAP_CCC		= (1 << 7),  /* Command Completion Coalescing */
+	HOST_CAP_PART		= (1 << 13), /* Partial state capable */
+	HOST_CAP_SSC		= (1 << 14), /* Slumber state capable */
+	HOST_CAP_PIO_MULTI	= (1 << 15), /* PIO multiple DRQ support */
+	HOST_CAP_FBS		= (1 << 16), /* FIS-based switching support */
+	HOST_CAP_PMP		= (1 << 17), /* Port Multiplier support */
+	HOST_CAP_ONLY		= (1 << 18), /* Supports AHCI mode only */
+	HOST_CAP_CLO		= (1 << 24), /* Command List Override support */
+	HOST_CAP_LED		= (1 << 25), /* Supports activity LED */
+	HOST_CAP_ALPM		= (1 << 26), /* Aggressive Link PM support */
+	HOST_CAP_SSS		= (1 << 27), /* Staggered Spin-up */
+	HOST_CAP_MPS		= (1 << 28), /* Mechanical presence switch */
+	HOST_CAP_SNTF		= (1 << 29), /* SNotification register */
+	HOST_CAP_NCQ		= (1 << 30), /* Native Command Queueing */
+	HOST_CAP_64		= (1 << 31), /* PCI DAC (64-bit DMA) support */
+
 	/* registers for each SATA port */
 	PORT_LST_ADDR		= 0x00, /* command list DMA addr */
 	PORT_LST_ADDR_HI	= 0x04, /* command list DMA addr hi */
@@ -58,6 +82,45 @@ enum {
 	PORT_SCR_NTF		= 0x3c, /* SATA phy register: SNotification */
 	PORT_FBS		= 0x40, /* FIS-based Switching */
 	PORT_DEVSLP		= 0x44, /* device sleep */
+
+	/* PORT_IRQ_{STAT,MASK} bits */
+	PORT_IRQ_COLD_PRES	= (1 << 31), /* cold presence detect */
+	PORT_IRQ_TF_ERR		= (1 << 30), /* task file error */
+	PORT_IRQ_HBUS_ERR	= (1 << 29), /* host bus fatal error */
+	PORT_IRQ_HBUS_DATA_ERR	= (1 << 28), /* host bus data error */
+	PORT_IRQ_IF_ERR		= (1 << 27), /* interface fatal error */
+	PORT_IRQ_IF_NONFATAL	= (1 << 26), /* interface non-fatal error */
+	PORT_IRQ_OVERFLOW	= (1 << 24), /* xfer exhausted available S/G */
+	PORT_IRQ_BAD_PMP	= (1 << 23), /* incorrect port multiplier */
+
+	PORT_IRQ_PHYRDY		= (1 << 22), /* PhyRdy changed */
+	PORT_IRQ_DEV_ILCK	= (1 << 7), /* device interlock */
+	PORT_IRQ_CONNECT	= (1 << 6), /* port connect change status */
+	PORT_IRQ_SG_DONE	= (1 << 5), /* descriptor processed */
+	PORT_IRQ_UNK_FIS	= (1 << 4), /* unknown FIS rx'd */
+	PORT_IRQ_SDB_FIS	= (1 << 3), /* Set Device Bits FIS rx'd */
+	PORT_IRQ_DMAS_FIS	= (1 << 2), /* DMA Setup FIS rx'd */
+	PORT_IRQ_PIOS_FIS	= (1 << 1), /* PIO Setup FIS rx'd */
+	PORT_IRQ_D2H_REG_FIS	= (1 << 0), /* D2H Register FIS rx'd */
+
+	/* PORT_CMD bits */
+	PORT_CMD_ASP		= (1 << 27), /* Aggressive Slumber/Partial */
+	PORT_CMD_ALPE		= (1 << 26), /* Aggressive Link PM enable */
+	PORT_CMD_ATAPI		= (1 << 24), /* Device is ATAPI */
+	PORT_CMD_FBSCP		= (1 << 22), /* FBS Capable Port */
+	PORT_CMD_PMP		= (1 << 17), /* PMP attached */
+	PORT_CMD_LIST_ON	= (1 << 15), /* cmd list DMA engine running */
+	PORT_CMD_FIS_ON		= (1 << 14), /* FIS DMA engine running */
+	PORT_CMD_FIS_RX		= (1 << 4), /* Enable FIS receive DMA engine */
+	PORT_CMD_CLO		= (1 << 3), /* Command list override */
+	PORT_CMD_POWER_ON	= (1 << 2), /* Power up device */
+	PORT_CMD_SPIN_UP	= (1 << 1), /* Spin up device */
+	PORT_CMD_START		= (1 << 0), /* Enable port DMA engine */
+
+	PORT_CMD_ICC_MASK	= (0xf << 28), /* i/f ICC state mask */
+	PORT_CMD_ICC_ACTIVE	= (0x1 << 28), /* Put i/f in active state */
+	PORT_CMD_ICC_PARTIAL	= (0x2 << 28), /* Put i/f in partial state */
+	PORT_CMD_ICC_SLUMBER	= (0x6 << 28), /* Put i/f in slumber state */
 
 };
 
