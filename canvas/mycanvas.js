4ef00325ad4a0bd05e2d1cdc9f1fc03ee5610ff2
Xun Gu <noguxun@gmail.com>
2014-05-26 21:25:01 +0900

    organize code in fancier way

--- canvas/mycanvas.js
+++ canvas/mycanvas.js
@@ -1,48 +1,56 @@
-My = {}
+var drawingApp = (function() {
+   "use strict";
+   
+   var canvas,
+   ctx,
+   
+   
+   log = function(x) {
+     console.log(x);
+   },
 
-function myMain()
-{
-  initCanvas();
-  drawSomething();
-}
+   initCanvas = function() {
+	 canvas = document.getElementById('myCanvas');
+	 ctx = canvas.getContext('2d');
+   },
 
-function initCanvas()
-{
-  My.canvas = document.getElementById('myCanvas');
-  My.context = My.canvas.getContext('2d');
-}
+   dot = function( x, y ){
+	 ctx.fillRect( x, y, 1, 1);
+   },
 
-function dot( x, y ) 
-{
-  var ctx = My.context;
-  ctx.fillRect( x, y, 1, 1);
-}
-
-function drawSomething()
-{
-  var ctx = My.context;
-  var i = 0;
-  var x, y, len, x1, y1
+   drawSomething = function(){
+	 var i = 0;
+	 var x, y, len, x1, y1
   
+	 i = 0;
+	 while( i < 361 ) {
+	   x = i;
+	   y = Math.sin( x * (Math.PI / 180)  ) * 60;
+	   dot( x/2, y + 100 );
+	   i ++
+	 }
+	 log("draw sin");
   
-  i = 0;
-  while( i < 361 ) {
-    x = i;
-    y = Math.sin( x * (Math.PI / 180)  ) * 60;
-    dot( x/2, y + 100 );
-    i ++
-  }
-  console.log("draw sin");
+	 x1 = 300;
+	 y1 = 300;
+	 len = 100
+	 i = 0;
+	 while( i < 900 ) {
+	   x = x1 + len * Math.cos( i/2 * (Math.PI / 180)  );
+	   y = y1 + len * Math.sin( i/2 * (Math.PI / 180)  );
+	   dot( x, y );
+	   i ++;
+	 }
+	 log("draw circle");
+   },
+   
+   mainExec = function() {
+	 initCanvas();
+	 drawSomething();
+   };
   
-  x1 = 300;
-  y1 = 300;
-  len = 100
-  i = 0;
-  while( i < 900 ) {
-    x = x1 + len * Math.cos( i/2 * (Math.PI / 180)  );
-    y = y1 + len * Math.sin( i/2 * (Math.PI / 180)  );
-    dot( x, y );
-    i ++;
-  }
-  console.log("draw circle");
-} 
+   return {
+	 main: mainExec
+   };
+
+}());
