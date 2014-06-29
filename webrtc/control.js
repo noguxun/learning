5a6bc240f75aefc11c8074343952f1023d1554da
Xun Gu <noguxun@gmail.com>
2014-06-29 18:49:50 +0900

    a more complete example

--- webrtc/control.js
+++ webrtc/control.js
@@ -1,41 +1,51 @@
-$(document).ready(function() {
-  var canvas;
-  var peerControl;
-  var connect;
+var controlApp = (function(){
 
-  peerControl = new Peer('whatthefuck_c', { key: '5xokte33u1ve7b9', debug: 3});
-  connect = peerControl.connect('whatthefuck');
+  var canvas,
+  peerControl,
+  connect, 
+  
+  
+  connectInit = function() {
+    peerControl = new Peer('whatthefuck_c', { key: '5xokte33u1ve7b9', debug: 3});
+    connect = peerControl.connect('whatthefuck');
 
-  connect.on('open', function() {
-    console.log("connection open");
-    connect.on('data', function(data) {
-      console.log("from server", data);
+    connect.on('open', function() {
+      console.log("connection open");
+      connect.on('data', function(data) {
+        console.log("from server", data);
+      });
     });
-    connect.send("hello");
-  });
+  }, 
+ 
+  mainExec = function() {
+    connectInit();
+    
+    canvas = document.getElementById('myCanvas');
+  
+  	canvas.addEventListener('touchmove', function(event) {
+      var rect = canvas.getBoundingClientRect();
+    
+      for (var i = 0; i < event.touches.length; i++) {
+        var touch = event.touches[i];
+        console.log(touch.pageX, touch.pageY);
+        var pos = {};
+        pos.x = touch.pageX - rect.left;
+        pos.y = touch.pageY - rect.top;
+        connect.send(pos);
+      }
+    });
+  
+  	canvas.addEventListener('touchstart', function(event) {
+  	  console.log("touchstart");
+  	});
+  	
+  	canvas.addEventListener('touchend', function() {
+      console.log("touchend");
+    });
+  };
 
-  //canvas = $("#mycanvas");
-  canvas = document.getElementById('mycanvas');
-  canvas.addEventListener('touchend', function() {
-    connect.send("touch end");
-    console.log("touchend");
-  });
+  return {
+    main: mainExec
+  };
 
-  canvas.addEventListener('touchmove', function(event) {
-    //connect.send(event.touches);
-    for (var i = 0; i < event.touches.length; i++) {
-      var touch = event.touches[i];
-      console.log(touch.pageX, touch.pageY);
-      var pos = {};
-      pos.x = touch.pageX;
-      pos.y = touch.pageY;
-      connect.send(pos);
-    }
-  });
-
-  canvas.addEventListener('touchstart', function(event) {
-    //connect.send(event);
-    console.log(event);
-  });
-
-});
+}());
--- webrtc/screen.js
+++ webrtc/screen.js
@@ -1,30 +1,62 @@
-$(document).ready(function() {
+var screenApp = (function(){
+  "use strict";
+  
+  var canvas,
+  ctx,
+  peerServer, 
+  
+  clearRect = function(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Store the current transformation matrix
+    ctx.save();
 
-  peerServer = new Peer('whatthefuck', { key: '5xokte33u1ve7b9', debug: 3});
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  console.log( "open server" );
-  peerServer.on('open', function(id) {
-    console.log( "server opened: " + id );
-  });
+    // Restore the transform
+    ctx.restore();
+  },
+  
+  
+  drawFinder = function(cx, cy, radius, color){
+    ctx.beginPath();
+    ctx.arc(cx, cy, radius, 0, 2*Math.PI, false);
+    ctx.moveTo(cx - radius - 4, cy);
+    ctx.lineTo(cx + radius + 4, cy);
+    ctx.moveTo(cx, cy - radius - 4);
+    ctx.lineTo(cx, cy + radius + 4);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+  },
+  
+  mainExec = function() {
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext('2d');
+    
+    peerServer = new Peer('whatthefuck', { key: '5xokte33u1ve7b9', debug: 3});
+    peerServer.on('open', function(id) {
+    	console.log( "server opened: " + id );
+  	});
+  	
+  	peerServer.on('connection', function(connection) {
+      console.log("connection from peer");
 
+      connection.on('open', function(){
+        console.log("connection opened");
+        connection.send("serverfunck you");
+      });
 
-
-  console.log( "call on connection 1" );
-
-  peerServer.on('connection', function(connection) {
-    console.log("connection from peer");
-
-    connection.on('open', function(){
-      console.log("connection opened");
-      connection.send("serverfunck you");
+      connection.on('data', function(data){
+        console.log( data );      
+        clearRect();
+        drawFinder( data.x, data.y, 10, "#0f0f0f" );
+      });
     });
+  };
 
-    connection.on('data', function(data){
-      console.log( data );
-    });
+  return {
+    main: mainExec
+  };
 
-  });
-
-  console.log( "call on connection 2" );
-
-});
+}());
