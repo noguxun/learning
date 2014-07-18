1ee357f79f54a97ce7c49d1a3afa489141ed515c
xun gu <noguxun@gmail.com>
2014-07-18 17:26:12 +0900

    works

--- blk_dev/sbull/bull.c
+++ blk_dev/sbull/bull.c
@@ -6,20 +6,18 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 
-
-#include <linux/slab.h>		/* kmalloc() */
-#include <linux/fs.h>		/* everything... */
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
 #include <linux/blkdev.h>
 #include <linux/bio.h>
+#include <linux/hdreg.h>
 
 MODULE_LICENSE("GPL");
 
-static int sbull_major = 0;
-module_param(sbull_major, int, 0);
+static int hx_major = 0;
 static int hardsect_size = 512;
-module_param(hardsect_size, int, 0);
-static int nsectors = 0x1000;	/* How big the drive is */
-module_param(nsectors, int, 0);
+static int nsectors = 0x100000;	/* How big the drive is */
 
 /*
  * We can tweak our hardware sector size, but the kernel talks to us
@@ -27,33 +25,28 @@ module_param(nsectors, int, 0);
  */
 #define KERNEL_SECTOR_SIZE	512
 
-#define PK(fmt, args...)  printk(KERN_INFO "sbull: " fmt, ## args )
-#define PKL(fmt, args...) printk(KERN_INFO "sbull: " fmt "\n", ## args )
+#define PK(fmt, args...)  printk(KERN_INFO "hx: " fmt, ## args )
+#define PKL(fmt, args...) printk(KERN_INFO "hx: " fmt "\n", ## args )
 
 /*
  * The internal representation of our device.
  */
-struct sbull_dev {
+struct hx_dev {
         int size;                       /* Device size in sectors */
         u8 *data;                       /* The data array */
-        short users;                    /* How many users */
-        short media_change;             /* Flag a media change? */
         spinlock_t lock;                /* For mutual exclusion */
         struct request_queue *queue;    /* The device request queue */
         struct gendisk *gd;             /* The gendisk structure */
-        struct timer_list timer;        /* For simulated media changes */
 };
 
-#if 1
-static struct sbull_dev *Device = NULL;
+static struct hx_dev *Device = NULL;
 
 /*
  * Handle an I/O request.
  */
-static void sbull_transfer(struct sbull_dev *dev, unsigned long sector,
+static void hx_transfer(struct hx_dev *dev, unsigned long sector,
 		unsigned long nsect, char *buffer, int write)
 {
-#if 0
 	unsigned long offset = sector*KERNEL_SECTOR_SIZE;
 	unsigned long nbytes = nsect*KERNEL_SECTOR_SIZE;
 
@@ -65,14 +58,13 @@ static void sbull_transfer(struct sbull_dev *dev, unsigned long sector,
 		memcpy(dev->data + offset, buffer, nbytes);
 	else
 		memcpy(buffer, dev->data + offset, nbytes);
-#endif
-	PKL("  xfer sector %lx nr %lx", sector, nsect);
+	PKL("  xfer sector %lx nr %lx wr %d offset %lx", sector, nsect, write, offset);
 }
 
 /*
  * Transfer a single BIO.
  */
-static int sbull_xfer_bio(struct sbull_dev *dev, struct bio *bio)
+static int hx_xfer_bio(struct hx_dev *dev, struct bio *bio)
 {
 	int i;
 	struct bio_vec *bvec;
@@ -81,7 +73,7 @@ static int sbull_xfer_bio(struct sbull_dev *dev, struct bio *bio)
 	/* Do each segment independently. */
 	bio_for_each_segment(bvec, bio, i) {
 		char *buffer = __bio_kmap_atomic(bio, i);
-		sbull_transfer(dev, sector, bio_cur_bytes(bio) >> 9,
+		hx_transfer(dev, sector, bio_cur_bytes(bio) >> 9,
 				buffer, bio_data_dir(bio) == WRITE);
 		sector += bio_cur_bytes(bio) >> 9;
 		__bio_kunmap_atomic(bio);
@@ -92,29 +84,26 @@ static int sbull_xfer_bio(struct sbull_dev *dev, struct bio *bio)
 /*
  * Transfer a full request.
  */
-static int sbull_xfer_request(struct sbull_dev *dev, struct request *req)
+static int hx_xfer_request(struct hx_dev *dev, struct request *req)
 {
 	struct bio *bio;
 	int nsect = 0;
-#if 0
+
 	__rq_for_each_bio(bio, req) {
-		sbull_xfer_bio(dev, bio);
+		hx_xfer_bio(dev, bio);
 		nsect += bio->bi_size/KERNEL_SECTOR_SIZE;
 	}
 	return nsect;
-#else
-	return 	(int)blk_rq_sectors(req);
-#endif
 }
 
 /*
  * Smarter request function that "handles clustering".
  */
-static void sbull_full_request(struct request_queue *q)
+static void hx_full_request(struct request_queue *q)
 {
 	struct request *req;
 	int sectors_xferred;
-	struct sbull_dev *dev = q->queuedata;
+	struct hx_dev *dev = q->queuedata;
 
 	while ((req = blk_fetch_request(q)) != NULL) {
 		if (req->cmd_type != REQ_TYPE_FS) {
@@ -122,12 +111,12 @@ static void sbull_full_request(struct request_queue *q)
 			__blk_end_request(req, -EIO, blk_rq_cur_bytes(req));
 			continue;
 		}
-		sectors_xferred = sbull_xfer_request(dev, req);
+		sectors_xferred = hx_xfer_request(dev, req);
 		PKL("request sector %lx nr %x xfered %x",
 			(unsigned long)blk_rq_pos(req),
 			(int)blk_rq_sectors(req),
-			sectors_xferred);
-		blk_end_request(req, 0, sectors_xferred);
+			sectors_xferred * hardsect_size);
+		__blk_end_request(req, 0, sectors_xferred*hardsect_size);
 	}
 }
 
@@ -135,25 +124,23 @@ static void sbull_full_request(struct request_queue *q)
 /*
  * Open and close.
  */
-static int sbull_open(struct block_device *bdev, fmode_t mode)
+static int hx_open(struct block_device *bdev, fmode_t mode)
 {
-	struct sbull_dev *dev = bdev->bd_disk->private_data;
+	struct hx_dev *dev = bdev->bd_disk->private_data;
 
 	spin_lock(&dev->lock);
-	dev->users++;
 	spin_unlock(&dev->lock);
-        PKL("open %d", dev->users);
+        PKL("open");
 	return 0;
 }
 
-static void sbull_release(struct gendisk *disk, fmode_t mode)
+static void hx_release(struct gendisk *disk, fmode_t mode)
 {
-	struct sbull_dev *dev = disk->private_data;
+	struct hx_dev *dev = disk->private_data;
 
 	spin_lock(&dev->lock);
-	dev->users--;
 	spin_unlock(&dev->lock);
-        PKL("release %d", dev->users);
+        PKL("release");
 
 	return;
 }
@@ -164,11 +151,25 @@ static void sbull_release(struct gendisk *disk, fmode_t mode)
  * The ioctl() implementation
  */
 
-int sbull_ioctl (struct block_device *bdev, fmode_t mode,
+int hx_ioctl (struct block_device *bdev, fmode_t mode,
                  unsigned int cmd, unsigned long arg)
 {
-	PKL("ioctl");
+	PKL("ioctl cmd %x", cmd);
 	return -ENOTTY; /* unknown command */
+}
+
+
+int hx_getgeo(struct block_device *bdev, struct hd_geometry *geo)
+{
+	long size = nsectors;
+
+	geo->cylinders = (size & ~0x3f) >> 6;
+	geo->heads = 4;
+	geo->sectors = 16;
+	//geo.start = 4;
+	PKL("getgeo");
+
+	return 0;
 }
 
 
@@ -176,23 +177,24 @@ int sbull_ioctl (struct block_device *bdev, fmode_t mode,
 /*
  * The device operations structure.
  */
-static struct block_device_operations sbull_ops = {
+static struct block_device_operations hx_ops = {
 	.owner           = THIS_MODULE,
-	.open 	         = sbull_open,
-	.release 	 = sbull_release,
-	.ioctl	         = sbull_ioctl
+	.open 	         = hx_open,
+	.release 	 = hx_release,
+	.ioctl	         = hx_ioctl,
+	.getgeo          = hx_getgeo,
 };
 
 
 /*
  * Set up our internal device.
  */
-static void setup_device(struct sbull_dev *dev)
+static void setup_device(struct hx_dev *dev)
 {
 	/*
 	 * Get some memory.
 	 */
-	memset (dev, 0, sizeof (struct sbull_dev));
+	memset (dev, 0, sizeof (struct hx_dev));
 	dev->size = nsectors*hardsect_size;
 	dev->data = vmalloc(dev->size);
 	if (dev->data == NULL) {
@@ -201,7 +203,7 @@ static void setup_device(struct sbull_dev *dev)
 	}
 	spin_lock_init(&dev->lock);
 
-	dev->queue = blk_init_queue(sbull_full_request, &dev->lock);
+	dev->queue = blk_init_queue(hx_full_request, &dev->lock);
 	if (dev->queue == NULL)
 		goto out_vfree;
 
@@ -216,13 +218,13 @@ static void setup_device(struct sbull_dev *dev)
 		printk (KERN_NOTICE "alloc_disk failure\n");
 		goto out_vfree;
 	}
-	dev->gd->major = sbull_major;
+	dev->gd->major = hx_major;
 	dev->gd->first_minor = 0;
-	dev->gd->fops = &sbull_ops;
+	dev->gd->fops = &hx_ops;
 	dev->gd->queue = dev->queue;
 	dev->gd->private_data = dev;
 	dev->gd->flags |= GENHD_FL_SUPPRESS_PARTITION_INFO;
-	snprintf (dev->gd->disk_name, 32, "sbull0");
+	snprintf (dev->gd->disk_name, 32, "hx");
 	set_capacity(dev->gd, nsectors*(hardsect_size/KERNEL_SECTOR_SIZE));
 	PKL("capacity %x", nsectors);
 	add_disk(dev->gd);
@@ -233,44 +235,37 @@ static void setup_device(struct sbull_dev *dev)
 		vfree(dev->data);
 }
 
-#endif
 
-
-static int __init sbull_init(void)
+static int __init hx_init(void)
 {
 	/*
 	 * Get registered.
 	 */
-#if 1
-	sbull_major = register_blkdev(sbull_major, "sbull");
-	if (sbull_major <= 0) {
+	hx_major = register_blkdev(hx_major, "hx");
+	if (hx_major <= 0) {
 		PKL("blk device registration failed\n");
 		return -EBUSY;
 	}
 	/*
 	 * Allocate the device array, and initialize each one.
 	 */
-	Device = kmalloc(sizeof (struct sbull_dev), GFP_KERNEL);
+	Device = kmalloc(sizeof (struct hx_dev), GFP_KERNEL);
 	if (Device == NULL)
 		goto out_unregister;
 
 	setup_device(Device);
-#endif
         PKL("init");
 
 	return 0;
 
-#if 1
   out_unregister:
-	unregister_blkdev(sbull_major, "sbull");
+	unregister_blkdev(hx_major, "hx");
 	return -ENOMEM;
-#endif
 }
 
-static void sbull_exit(void)
+static void hx_exit(void)
 {
-#if 1
-	struct sbull_dev *dev = Device;
+	struct hx_dev *dev = Device;
 
 	if (dev->gd) {
 		del_gendisk(dev->gd);
@@ -282,11 +277,10 @@ static void sbull_exit(void)
 	if (dev->data)
 		vfree(dev->data);
 
-	unregister_blkdev(sbull_major, "sbull");
+	unregister_blkdev(hx_major, "hx");
 	kfree(Device);
-#endif
 	PKL("exit");
 }
 
-module_init(sbull_init);
-module_exit(sbull_exit);
+module_init(hx_init);
+module_exit(hx_exit);
