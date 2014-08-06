8c83f1f7fa95fdbfa99177f91905df71d28ce4fc
Xun Gu <yesguxun@gmail.com>
2014-08-06 15:15:23 +0900

    this is a framwork for blk device with async operation

--- blk_dev/hellx/hellx.c
+++ blk_dev/hellx/hellx.c
@@ -12,6 +12,7 @@
 #include <linux/blkdev.h>
 #include <linux/bio.h>
 #include <linux/hdreg.h>
+#include <linux/workqueue.h>
 
 MODULE_LICENSE("GPL");
 
@@ -31,12 +32,17 @@ static int nsectors = 0x100000;	/* How big the drive is */
 /*
  * The internal representation of our device.
  */
+/*
+ * TODO: need to add a mutex protection in process context
+ */
 struct hx_dev {
         int size;                       /* Device size in sectors */
         u8 *data;                       /* The data array */
         spinlock_t lock;                /* For mutual exclusion */
         struct request_queue *queue;    /* The device request queue */
         struct gendisk *gd;             /* The gendisk structure */
+	struct workqueue_struct *wq;    /* The work queue */
+	struct work_struct work;        /* The work object */
 };
 
 static struct hx_dev *Device = NULL;
@@ -51,7 +57,7 @@ static void hx_transfer(struct hx_dev *dev, unsigned long sector,
 	unsigned long nbytes = nsect*KERNEL_SECTOR_SIZE;
 
 	if ((offset + nbytes) > dev->size) {
-		printk (KERN_NOTICE "Beyond-end write (%ld %ld)\n", offset, nbytes);
+		PKL("Beyond-end write (%ld %ld)\n", offset, nbytes);
 		return;
 	}
 	if (write)
@@ -99,25 +105,11 @@ static int hx_xfer_request(struct hx_dev *dev, struct request *req)
 /*
  * Smarter request function that "handles clustering".
  */
-static void hx_full_request(struct request_queue *q)
+static void hx_request(struct request_queue *q)
 {
-	struct request *req;
-	int sectors_xferred;
 	struct hx_dev *dev = q->queuedata;
 
-	while ((req = blk_fetch_request(q)) != NULL) {
-		if (req->cmd_type != REQ_TYPE_FS) {
-			printk (KERN_NOTICE "Skip non-fs request\n");
-			__blk_end_request(req, -EIO, blk_rq_cur_bytes(req));
-			continue;
-		}
-		sectors_xferred = hx_xfer_request(dev, req);
-		PKL("request sector %lx nr %x xfered %x",
-			(unsigned long)blk_rq_pos(req),
-			(int)blk_rq_sectors(req),
-			sectors_xferred * hardsect_size);
-		__blk_end_request(req, 0, sectors_xferred*hardsect_size);
-	}
+	queue_work(dev->wq, &dev->work);
 }
 
 
@@ -145,6 +137,45 @@ static void hx_release(struct gendisk *disk, fmode_t mode)
 	return;
 }
 
+static void hx_work(struct work_struct *work)
+{
+	struct hx_dev *dev =
+		container_of(work, struct hx_dev, work);
+	struct request_queue *rq = dev->queue;
+	struct request *req;
+	int sectors_xferred;
+
+
+	while (1) {
+		spin_lock_irq(rq->queue_lock);
+		req = blk_fetch_request(rq);
+		spin_unlock_irq(rq->queue_lock);
+
+		if (req == NULL) {
+			break;
+		}
+
+		if (req->cmd_type != REQ_TYPE_FS) {
+			PKL("Skip non-fs request\n");
+
+			spin_lock_irq(rq->queue_lock);
+			__blk_end_request(req, -EIO, blk_rq_cur_bytes(req));
+			spin_unlock_irq(rq->queue_lock);
+
+			continue;
+		}
+		sectors_xferred = hx_xfer_request(dev, req);
+		PKL("request sector %lx nr %x xfered %x",
+			(unsigned long)blk_rq_pos(req),
+			(int)blk_rq_sectors(req),
+			sectors_xferred * hardsect_size);
+
+		spin_lock_irq(rq->queue_lock);
+		__blk_end_request(req, 0, sectors_xferred*hardsect_size);
+		spin_unlock_irq(rq->queue_lock);
+	}
+}
+
 
 
 /*
@@ -168,27 +199,67 @@ static struct block_device_operations hx_ops = {
 	.ioctl	         = hx_ioctl,
 };
 
+static void clean_up(struct hx_dev *dev)
+{
+	if (dev->gd) {
+		del_gendisk(dev->gd);
+		put_disk(dev->gd);
+	}
+
+	if (dev->queue) {
+		blk_cleanup_queue(dev->queue);
+	}
+
+	if (dev->data){
+		vfree(dev->data);
+	}
+
+	if (dev->wq){
+		destroy_workqueue(dev->wq);
+	}
+}
+
 
 /*
  * Set up our internal device.
  */
-static void setup_device(struct hx_dev *dev)
+static int setup_device(struct hx_dev *dev)
 {
+	int ret = 0;
+
 	/*
-	 * Get some memory.
+	 * Alloc memory for storage
 	 */
 	memset (dev, 0, sizeof (struct hx_dev));
 	dev->size = nsectors*hardsect_size;
 	dev->data = vmalloc(dev->size);
 	if (dev->data == NULL) {
-		printk (KERN_NOTICE "vmalloc failure.\n");
-		return;
+		PKL("vmalloc failure.");
+		ret = -ENOMEM;
+		goto END;
 	}
+
+	/*
+	 * Initialize the work queue
+	 */
+	dev->wq = create_workqueue("hellx_wq");
+	if (dev->wq == NULL) {
+		PKL("Work queue creation failure.");
+		ret = -ENOMEM;
+		goto END;
+	}
+	INIT_WORK(&dev->work, hx_work);
+
+	/*
+	 * Init the lock used by request queue
+	 */
 	spin_lock_init(&dev->lock);
 
-	dev->queue = blk_init_queue(hx_full_request, &dev->lock);
-	if (dev->queue == NULL)
-		goto out_vfree;
+	dev->queue = blk_init_queue(hx_request, &dev->lock);
+	if (dev->queue == NULL) {
+		ret = -ENOMEM;
+		goto END;
+	}
 
 	blk_queue_logical_block_size(dev->queue, hardsect_size);
 	dev->queue->queuedata = dev;
@@ -197,28 +268,29 @@ static void setup_device(struct hx_dev *dev)
 	 * And the gendisk structure.
 	 */
 	dev->gd = alloc_disk(1);
-	if (! dev->gd) {
-		printk (KERN_NOTICE "alloc_disk failure\n");
-		goto out_vfree;
+	if (!dev->gd) {
+		PKL("alloc_disk failure");
+		ret = -ENOMEM;
+		goto END;
 	}
 	dev->gd->major = hx_major;
 	dev->gd->first_minor = 0;
 	dev->gd->fops = &hx_ops;
 	dev->gd->queue = dev->queue;
 	dev->gd->private_data = dev;
-	/*
-	 * Not needed?
-	 * dev->gd->flags |= GENHD_FL_SUPPRESS_PARTITION_INFO;
-	 */
 	snprintf (dev->gd->disk_name, 32, "hellx");
 	set_capacity(dev->gd, nsectors*(hardsect_size/KERNEL_SECTOR_SIZE));
 	PKL("capacity %x", nsectors);
 	add_disk(dev->gd);
-	return;
 
-  out_vfree:
-	if (dev->data)
-		vfree(dev->data);
+	ret = 0;
+
+  END:
+  	if (ret != 0 ) {
+		clean_up(dev);
+	}
+
+	return ret;
 }
 
 
@@ -253,16 +325,7 @@ static void hx_exit(void)
 {
 	struct hx_dev *dev = Device;
 
-	if (dev->gd) {
-		del_gendisk(dev->gd);
-		put_disk(dev->gd);
-	}
-	if (dev->queue) {
-		blk_cleanup_queue(dev->queue);
-	}
-	if (dev->data)
-		vfree(dev->data);
-
+	clean_up(dev);
 	unregister_blkdev(hx_major, "hx");
 	kfree(Device);
 	PKL("exit");
